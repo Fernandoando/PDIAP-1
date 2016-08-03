64b9dff5ccde5d4bd7adb72353ffcd01a5bb3586
tiagoportaluppi <tiagoportaluppi.tp@gmail.com>
2016-08-03 17:06:44 -0300

    Rotas ok

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 ,	router = express.Router();
 
 //GET na homepage (/).
-router.get('*', function(req, res) {
-  res.render('layout');
+router.all('/', function(req, res) {
+  res.render('layout.ejs');
 });
 
 module.exports = router;
